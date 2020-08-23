a224c36015fbaf2a8ee605a28c93a193dc5782c4
ray-yhc <raycho521@yonsei.ac.kr>
2020-08-23 14:09:19 +0900

    200823_3

---
+++ Data_and_Operation_2nd/src/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("helloWorld"); //≈´µ˚ø»«• : String
+		System.out.println('H'); //¿€¿∫µ˚ø»«• : Character
+		
+		System.out.println("Hello "
+				+ "World"); //enter -> +∑Œ ¿⁄µø ¡ŸπŸ≤ﬁ «ÿ¡‹
+		System.out.println("Hello \nWorld");
+		System.out.println("Hello \"World\"");
+		
+		System.out.println("\n\n");
+		
+		System.out.println("Hello World".length());
+		System.out.println("Hello Cho, ... bye".replace("Cho", "Yoon"));
+	}
+
+}
---
+++ Data_and_Operation_2nd/src/Variable.java
@@ -0,0 +1,8 @@
+
+public class Variable {
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println(a);
+		
+	}
+}
